fe1328a6d06cccbc3e4271f53f8d6ea43f340390
Adrian <pogorzelski.adrian@gmail.com>
2023-02-12 12:33:33 +0100

    Zadanie 2 gotowe

--- praca-domowa-2/script.js
+++ praca-domowa-2/script.js
@@ -1,16 +1,7 @@
-const content = document.getElementById('content')
-const translateText = () => {
-    const inputText = document.getElementById('inputText').value
-    const targetDiv = document.getElementById('content')
-    axios.get('https://libretranslate.com/translate?q=hello&source=en&target=cs')
-        .then(response => targetDiv.innerHTML = response.translatedText)
-        .catch(targetDiv.innerHTML = '???')
-/*    const header = {
-        'q': inputText,
-        'source': 'pl',
-        'target': 'cs'
-    }
-    axios.post('https://libretranslate.com/translate', header)
-        .then(response => targetDiv.innerHTML = response.translatedText)
-        .catch(targetDiv.innerHTML = '???')*/
+const resultDiv = document.getElementById('result')
+const checkYear = () => {
+    const input = document.getElementById('number').value
+    axios.get(`http://numbersapi.com/${input}/year`)
+        .then(response => resultDiv.innerHTML = response.data)
+        .catch(error => resultDiv.innerHTML = "ERROR: Incorrect date (" + error.message + ")!")
 }
